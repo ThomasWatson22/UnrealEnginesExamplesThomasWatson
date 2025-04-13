36fea60cf5565800a512dcaa9e0456e52ba34bcd
ThomasWatson22 <101474606@georgebrown.ca>
2025-04-13 16:48:08 -0400

    add

---
+++ GAME2020_M2_Watson_Thomas_CarCombatGame/Source/CarCombtGame/CarCombtGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CarCombtGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CarCombtGame, "CarCombtGame" );
